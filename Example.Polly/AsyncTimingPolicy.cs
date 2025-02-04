ff9aeea4bebd143b6df71d7470f29cd6bbe2db10
Julian Pritchard <Julian@Pritchard.email>
2025-02-04 07:47:02 +0200

    Polly Example

---
+++ Example.Polly/AsyncTimingPolicy.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Polly;
+
+namespace Example.Polly;
+
+public class AsyncTimingPolicy<TResult> : AsyncPolicy<TResult>
+{
+    private readonly Func<TimeSpan, Context, Task> _timingPublisher;
+
+    public AsyncTimingPolicy(Func<TimeSpan, Context, Task> timingPublisher)
+    {
+        _timingPublisher = timingPublisher;
+    }
+
+    public static AsyncTimingPolicy<TResult> Create(Func<TimeSpan, Context, Task> timingPublisher)
+        => new AsyncTimingPolicy<TResult>(timingPublisher);
+
+    protected override async Task<TResult> ImplementationAsync
+    (
+        Func<Context, CancellationToken, Task<TResult>> action,
+        Context context,
+        CancellationToken cancellationToken,
+        bool continueOnCapturedContext
+    )
+    {
+        var start = Stopwatch.GetTimestamp();
+
+        try
+        {
+            return await action.Invoke(context, cancellationToken).ConfigureAwait(continueOnCapturedContext);
+        }
+        finally
+        {
+            var elapsed = Stopwatch.GetElapsedTime(start);
+            await _timingPublisher.Invoke(elapsed, context).ConfigureAwait(continueOnCapturedContext);
+        }
+    }
+}
--- Example.Polly/Program.cs
+++ Example.Polly/Program.cs
@@ -1,11 +1,21 @@
+using Example.Polly;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Polly;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string TestClient = "TestClient";
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient(TestClient)
+    .AddPolicyHandler(AsyncTimingPolicy<HttpResponseMessage>.Create(PublishTiming));
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,7 +46,40 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
+app.MapGet("/do-thing", async () =>
+{
+    var client = app.Services.GetRequiredService<IHttpClientFactory>().CreateClient(TestClient);
+    string[] urls =
+    [
+        "https://www.google.com/",
+        "https://www.google.co.uk/",
+        "https://www.bbc.co.uk/"
+    ];
+
+    foreach (var url in urls)
+    {
+        var context = new Context {["url"] = url};
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.SetPolicyExecutionContext(context);
+
+        var result = await client.SendAsync(request, CancellationToken.None);
+    }
+    await Task.CompletedTask;
+})
+.WithName("DoThing")
+.WithOpenApi();
+
 app.Run();
+
+static Task PublishTiming(TimeSpan executionDuration, Context context)
+{
+    object url = "[unknown]";
+    if (context?.TryGetValue("url", out url) ?? false)
+    {
+        Console.WriteLine($"Took {executionDuration.TotalSeconds:0.###} seconds retrieving {url}");
+    }
+    return Task.CompletedTask;
+}
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
